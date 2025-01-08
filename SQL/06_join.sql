083b7536d0b0a58966e1fb58c3e8116503987220
Sumin <emily9949@naver.com>
2025-01-08 11:03:20 +0900

    [#2] SQL 내장 함수 업로드, join 파일 수정

--- SQL/06_join.sql
+++ SQL/06_join.sql
@@ -1,126 +1,78 @@
--- 1)
-SELECT *
-	FROM employee;
-	
--- 2)
-SELECT emp_id, emp_name
-	FROM employee;
-	
--- 3)
-SELECT emp_id, emp_name
-	FROM employee
-	WHERE emp_id = '201';
-	
--- 4)
-SELECT emp_name, dept_code
-	FROM employee
-	WHERE dept_code = 'D9';
-	
--- 5)
-SELECT emp_name, job_code
-	FROM employee
-	WHERE job_code = 'J1'
-	
--- 6)
-SELECT emp_id, emp_name, dept_code, salary
-	FROM employee
-	WHERE salary >= 3000000;
-	
--- 7)
-SELECT emp_name, dept_code, salary
-	FROM employee
-	WHERE dept_code = 'D6' AND salary >= 3000000;
-	
--- 8)
-SELECT emp_name, emp_id, salary, bonus
-	FROM employee
-	WHERE bonus IS NULL;
-	
--- 9)
-SELECT emp_id, emp_name, dept_code
-	FROM employee
-	WHERE dept_code != 'D9';	
-		
--- 10)
-SELECT emp_id AS '사번'
-		, emp_name AS '이름'
-		, hire_date AS '입사일'
-	FROM employee
-	WHERE ent_yn = 'N';
-	
--- 11) 
-SELECT 
-		 emp_name AS '이름'
-	  , emp_id AS '사번'
-	  , salary AS '급여'
-	  , dept_code AS '부서코드'
-	  , job_code AS '직급코드'
-	  FROM employee
-	  WHERE salary BETWEEN 3500000 AND 5500000;
-	  
--- 12)
-SELECT emp_name
-		,emp_id
-		 ,hire_date
-		 FROM employee
-		 WHERE emp_name LIKE '김%';
-		 
--- 13)
-SELECT 
-		 emp_name
-		,emp_id
-		,hire_date
- FROM employee
-WHERE emp_name NOT LIKE '김%';
-		 
--- 14)
-SELECT 
-		  emp_name
-		, emp_no
-		 , dept_code
-  FROM employee
-WHERE emp_name LIKE '%하%';
+-- join
 
--- 15)
-SELECT 
-		 emp_name
-		, salary
-		, job_code
-FROM employee
-WHERE job_code = 'J7' OR (salary >= 2000000 AND job_code = 'J2'); 
+SELECT * FROM tbl_menu;
+SELECT * FROM tbl_category;
 
-
--- 16)
+-- 별칭 안 적었을 때
 SELECT
-		 emp_name
-		, salary
-		, job_code
-FROM employee
-WHERE salary >= 2000000 AND (job_code = 'J7' OR job_code = 'J2');
-
-	
--- 17)
+       menu_name
+     , category_name
+--      , category_code		-- 이 부분은 ambiguos 에러 발생
+  FROM tbl_menu 
+  JOIN tbl_category ON tbl_menu.category_code = tbl_category.category_code;
+  
+-- 별칭 작성 시
+-- 테이블 또는 from 절에 별칭을 추가할 때는 mariadb는 싱글쿼테이션(') 없이 별칭을
+-- 작성해야 한다.(as는 생략이 가능하다)
 SELECT
-		 emp_name
-		, salary
-		, job_code
-FROM employee
-WHERE salary >= 2000000 AND job_code IN ('J7', 'J2');	
-	
+       a.menu_name AS '메뉴명'
+     , b.category_name
+     , a.category_code
+  FROM tbl_menu a
+  JOIN tbl_category b ON a.category_code = b.category_code;
+  
+-- --------------------------------------------------------------------
+-- inner join
+-- 1)on 활용(join할 조건 작성)(컬럼명이 다를 때는 반드시 on을 작성)
+SELECT
+       a.menu_name AS '메뉴명'
+     , b.category_name
+     , a.category_code
+  FROM tbl_menu a
+ inner JOIN tbl_category b ON a.category_code = b.category_code;
 
--- -----------------------------------------
--- join 문제
+-- 2)using 활용(컬럼명이 같을 때는 using을 활용해서 짧은 구문으로 작성 가능)
+SELECT
+       a.menu_name AS '메뉴명'
+     , b.category_name
+     , a.category_code
+  FROM tbl_menu a
+ inner JOIN tbl_category b USING (category_code)
+ ORDER BY category_code asc;
+ 
+SELECT * FROM tbl_category;
 
--- 1)
+-- --------------------------------------------------------------
+-- outer join
+-- 1) left join
+SELECT
+       a.category_name
+     , b.menu_name
+  FROM tbl_category a 
+  LEFT JOIN tbl_menu b ON a.category_code = b.category_code;
 
-
-
-
-
-
-
-
-	
-	
-	
-	
+-- 2) right join
+SELECT
+       b.category_name
+     , a.menu_name
+  FROM tbl_menu a
+ RIGHT JOIN tbl_category b ON a.category_code = b.category_code;
+ 
+-- 3) cross join
+SELECT
+       *
+  FROM tbl_menu a
+ CROSS JOIN tbl_category b;
+ 
+-- 4) self join
+SELECT
+       a.category_name
+     , b.category_name
+  FROM tbl_category a
+  JOIN tbl_category b ON (a.ref_category_code = b.category_code);
+  
+  
+SELECT
+       *
+  FROM tbl_menu a
+  JOIN tbl_category b ON (a.category_code = b.category_code);
