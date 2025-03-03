d581fbd6c49436c84efd0317df442a7591a80fcd
Sumin <emily9949@naver.com>
2025-03-03 10:00:00 +0900

    프로그래머스 야근지수 문제풀이

---
+++ 프로그래머스/src/PS12927.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class PS12927 {
+    public long solution(int n, int[] works) {
+            long answer = 0;
+            PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+            for(int w : works)
+                queue.add(w);
+            for(int i = 0; i < n; i++)
+                queue.offer(queue.poll() - 1);
+
+            for(int q : queue)
+                if(q > 0)
+                    answer += q * q;
+            return answer;
+    }
+}
+
